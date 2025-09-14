3303be142be324e4021bb821dc1116f87346eb04
DAYNLIGHT (✿◡‿◡) <vampdaynlight@gmail.com>
2025-09-14 02:09:21 +0200

    add license on top

--- DSL/DSL/dsl.h
+++ DSL/DSL/dsl.h
@@ -1,3 +1,26 @@
+/*
+ * MyProgram - short description
+ * Copyright (C) 2025 Your Name
+ *
+ * This file is part of MyProgram.
+ *
+ * MyProgram is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MyProgram is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+ 
+
 #ifndef PragmaDSL
 #define PragmaDSL
 
