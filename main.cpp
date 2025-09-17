8fd6b4cfae7f188796aebbc3c2d483f3ff14db34
DAYNLIGHT (✿◡‿◡) <vampdaynlight@gmail.com>
2025-09-18 01:50:19 +0200

    rm modulo tests

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include "DSL/dsl.h"
 #include "Tests/Unit/unit_tests.h"
 #include "Examples/Essentials/Vector/vector.h"
-#include "Examples/Math/ModuloZ/moduloz.h"
 
 int main(){
   UnitTests::AllTests();
