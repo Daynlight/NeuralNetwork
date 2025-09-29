d12a9efb823089579a5d24e30f8aa811d4edaef6
daynlight <danielstodulski000@gmail.com>
2025-09-29 14:28:48 +0200

    smaller input matrix

--- Examples/NeuralNetwork/neuralnetwork.h
+++ Examples/NeuralNetwork/neuralnetwork.h
@@ -6,7 +6,7 @@ void NeuralNetwork() {
   fmt::print(fg(fmt::color::aquamarine) , "=== NeuralNetwork Example ===\n");
 
   // create Layer
-  NN::Layer<2, 2> layer;
+  NN::Layer<1, 1> layer;
   fmt::print("{}\n", layer.print());
 
   fmt::print("\n\n");
